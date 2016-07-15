706762ef5eb476c5ee98032ef9b69445a79bf46b
Marcelo Contreras <marcelojoc@hotmail.com>
2016-07-15 06:49:00 -0300

    backend inicial login usuario 1º commit

---
+++ application/views/backend/login_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login del sitio_view </title>
+  </head>
+  <body>
+    <p>
+      Login del sitio....aqui va el formulario y eso...
+
+    </p>
+  </body>
+</html>
